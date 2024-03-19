39fc0f953d369dbdd541a96c861638befa6f8b45
Ekrem A <Altuntop@stud.uni-heidelberg.de>
2024-03-19 14:29:40 +0000

    Updated fp* instructions in util.h, some testing, successful compilations NOT guaranteed

--- soc_frame/sw/controller/scheduler/prgs.h
+++ soc_frame/sw/controller/scheduler/prgs.h
@@ -1,25 +1,16 @@
 /* This file has been automatically generated */
 
-#define NUM_PRGS ( 2 )
+#define NUM_PRGS ( 1 )
 
 void init_prgs( prg_t *prgs )
 {
-	prgs[0].name = "basicmath";
-	prgs[0].addr[ARCH_RV32IM] = 0x1fff0; // index: 32764 size: 346992
+	prgs[0].name = "fpmul";
+	prgs[0].addr[ARCH_RV32IM] = 0x1fff0; // index: 32764 size: 4040
 	prgs[0].exec_t[ARCH_RV32IM] = 0;
 	prgs[0].exec_inv[ARCH_RV32IM] = 0;
 	prgs[0].exec_e[ARCH_RV32IM] = 0;
 	prgs[0].skip_after    = 5;
 	prgs[0].skip_cnt_down = 5;
 	prgs[0].overflows = 0;
-
-	prgs[1].name = "msort";
-	prgs[1].addr[ARCH_RV32IM] = 0x2c9268; // index: 730266 size: 3340
-	prgs[1].exec_t[ARCH_RV32IM] = 0;
-	prgs[1].exec_inv[ARCH_RV32IM] = 0;
-	prgs[1].exec_e[ARCH_RV32IM] = 0;
-	prgs[1].skip_after    = 5;
-	prgs[1].skip_cnt_down = 5;
-	prgs[1].overflows = 0;
 
 }
